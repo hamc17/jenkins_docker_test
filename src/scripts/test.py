a5a6ae2dd6a519751692429210b98b8a552545ae
Harry McLaughlin (ZA) <ABHM721@corp.dsarena.com>
2018-12-19 13:09:09 +0100

    Test Build

--- src/scripts/test.py
+++ src/scripts/test.py
@@ -1,17 +1,22 @@
 import time
 
 
-def fib():
-    count = 0
+def epoch_fib_count():
+    """
+    Gets the number of of the first fibonacci number greater
+    than the current time in epoch seconds
+    """
+
+    count = 2
+    # initial numbers need to be set as 0 & 1
     prev = [0, 1]
     t = int(time.time())
+    n = 0
     while prev[-1] < t:
         n = prev[-1] + prev[-2]
         count += 1
-	prev[0], prev[1] = prev[1], n
-    return count
-      
+    prev[0], prev[1] = prev[1], n
+    return count, n
+
 if __name__ == "__main__":
-    print "this is a test"
-    print fib()
-    print "hook works"
+    print epoch_fib_count()
