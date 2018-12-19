e67c8364eba2247b93a15b41c82e163e9c9b5bee
Harry McLaughlin (ZA) <ABHM721@corp.dsarena.com>
2018-12-19 10:58:18 +0100

    Updated test

--- src/scripts/test.py
+++ src/scripts/test.py
@@ -1,1 +1,16 @@
-print "this is a test"
+import time
+
+
+def fib():
+    count = 0
+    prev = [0, 1]
+    t = int(time.time())
+    while prev[-1] < t:
+        n = prev[-1] + prev[-2]
+        count += 1
+	prev[0], prev[1] = prev[1], n
+    return count
+      
+if __name__ == "__main__":
+    print "this is a test"
+    print fib()
